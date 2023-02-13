39417e7dae623dfc8d647cb0e91b0575a0ad5401
CUBASAO <dayronjosegonzalez@gmail.com>
2023-02-13 17:29:28 +0100

    empezando evento lista

--- javascript/app_audio.js
+++ javascript/app_audio.js
@@ -2,50 +2,55 @@ const button = document.querySelectorAll(".album");
 let a = new Array();
 var repro_canciones;
 var caja_canciones;
-
+let div_caja1 = document.querySelector(".caja1 ");
+var tema = document.querySelector("#cancion");
+// tema.volume = 0.5;
 Array.from(document.querySelectorAll(".album")).forEach((element) => {
   element.firstElementChild.addEventListener("click", function () {
-    alert("Hello!");
     let comparativa = element.lastElementChild.innerText;
-    let clase_lista = document.querySelector(".lista");
+    // let clase_lista = document.querySelector(".lista");
     fetch("./Json/albunes.json")
       .then((data) => data.json())
       .then((data) => {
+        alert("Creando Reproductor de : " + comparativa);
         for (let index = 0; index < data.albunes.length; index++) {
           if (comparativa == data.albunes[index].album) {
-            let repro_canciones = document.querySelector(".reproductor");
+            // let repro_canciones = document.querySelector(".reproductor");
             if (document.querySelector(".lista")) {
               var d_nested = document.querySelector(".lista");
-              repro_canciones.removeChild(d_nested);
+              div_caja1.removeChild(d_nested);
               var d_nested_2 = document.querySelector(".cromo");
               while (d_nested_2.firstChild) {
                 d_nested_2.removeChild(d_nested_2.firstChild);
               }
-              let as = document.querySelector("body");
+              let as = document.querySelector(".reproductor");
               var banner = document.querySelector(".crearbanner");
               as.removeChild(banner);
-
-              // repro_canciones.removeChild(d_nested_2);
+              /*Creacion reproductor cancion*/
+              crearReproductor(data, comparativa);
               crearLista(
                 data.albunes[index].canciones.length,
                 index,
                 data,
-                repro_canciones
+                div_caja1
               );
-              crearReproductor(data, comparativa);
-              crearbannerRepro();
 
-              /*Creacion reproductor cancion*/
+              crearbannerRepro(data, comparativa);
+              // eventos_lista(comparativa, data);
+              eventos_lista(data, comparativa);
             } else {
+              /*Creacion reproductor cancion*/
+              crearReproductor(data, comparativa);
               crearLista(
                 data.albunes[index].canciones.length,
                 index,
                 data,
-                repro_canciones
+                div_caja1
               );
-              /*Creacion reproductor cancion*/
-              crearReproductor(data, comparativa);
-              crearbannerRepro();
+
+              crearbannerRepro(data, comparativa);
+              // eventos_lista(comparativa, data);
+              eventos_lista(data, comparativa);
             }
           }
         }
@@ -53,7 +58,7 @@ Array.from(document.querySelectorAll(".album")).forEach((element) => {
   });
 });
 
-function crearLista(numero_array, index, data, repro_canciones) {
+function crearLista(numero_array, index, data, div_caja1) {
   caja_canciones = document.createElement("div");
   caja_canciones.classList.add("lista");
 
@@ -67,25 +72,20 @@ function crearLista(numero_array, index, data, repro_canciones) {
     let li_lista = document.createElement("li");
     li_lista.setAttribute("style", "color:white");
     li_lista.classList.add("btn_cancion");
-
     li_lista.append(data.albunes[index].canciones[i].titulo);
-    li_lista.addEventListener("click", () => {
-      console.log("hola");
-    });
     ul_lista.appendChild(li_lista);
   }
   titulo.append(data.albunes[index].album);
   caja_canciones.appendChild(titulo);
   caja_canciones.appendChild(ul_lista);
 
-  repro_canciones.appendChild(caja_canciones);
+  div_caja1.appendChild(caja_canciones);
 }
 
 function crearReproductor(data, comparativa) {
   comparativa = comparativa + ".jpg";
 
   for (let index = 0; index < data.albunes.length; index++) {
-    // console.log(data.albunes[index].portada);
     if (comparativa == data.albunes[index].portada) {
       let contenedor_cromo = document.querySelector(".cromo");
       let cromo_img = document.createElement("img");
@@ -98,44 +98,96 @@ function crearReproductor(data, comparativa) {
   }
 }
 function crearbannerRepro() {
-  console.log("crearbannerRepro");
   let cuerpo = document.querySelector("body");
   let pie = document.querySelector("footer");
 
-  let seccion = document.createElement("section");
-  seccion.classList.add("crearbanner");
+  // let seccion = document.createElement("section");
+  // seccion.classList.add("crearbanner");
   let contenido = document.createElement("div");
+  contenido.classList.add("crearbanner");
 
-  contenido.innerHTML = ` <div>
-  <h2 id="seleccionada"></h2>
-  <input id="barra" type="range" name="" min="" value="0" />
-</div>
-<div class="flex controles">
-  <div>
-    <img id="anterior" src="./images/cancion_anterior.svg" alt=" " />
-    <img id="play" src="./images/play.svg" alt=" " />
-    <img id="stop" src="./images/stop.svg" alt="" />
-    <img id="siguiente" src="./images/cancion_siguiente.svg" alt="" />
-    <img id="loop" src="./images/repetir.svg" alt="" />
-    <img id="random" src="./images/random.svg" alt="" />
+  contenido.innerHTML = ` 
+  <div id="contenedor_barra">
+    <h2 id="seleccionada"></h2>
+    <input id="barra" type="range" name="" min="" value="0" />
   </div>
-  <div class="flex">
-    <img id="mute" src="./images/volumen_menos.svg" alt=" " />
-    <input
-      id="volumen"
-      type="range"
-      name=""
-      min="0"
-      max="1"
-      step="any"
-    />
-    <img id="volum_max" src="./images/volumen_mas.svg" alt="" />
+  <div class="flex controles">
+    <div>
+      <img id="anterior" src="./images/cancion_anterior.svg" alt=" " />
+      <img id="play" src="./images/play.svg" alt=" " />
+      <img id="stop" src="./images/stop.svg" alt="" />
+      <img id="siguiente" src="./images/cancion_siguiente.svg" alt="" />
+      <img id="loop" src="./images/repetir.svg" alt="" />
+      <img id="random" src="./images/random.svg" alt="" />
+    </div>
+    <div class="flex">
+      <img id="mute" src="./images/volumen_menos.svg" alt=" " />
+      <input
+        id="volumen"
+        type="range"
+        name=""
+        min="0"
+        max="1"
+        step="any"
+      />
+      <img id="volum_max" src="./images/volumen_mas.svg" alt="" />
+    </div>
   </div>
-</div>
+
   `;
-  // console.log(contenido);
 
-  seccion.append(contenido);
-  console.log(seccion);
-  cuerpo.insertBefore(seccion, pie);
+  let a = document.querySelector(".reproductor");
+  a.appendChild(contenido);
 }
+function eventos_lista(data, comparativa) {
+  // comparativa, data
+  let lista_accion = document.querySelectorAll(".btn_cancion");
+  for (let index = 0; index < lista_accion.length; index++) {
+    lista_accion[index].addEventListener("click", function () {
+      añadir_audio(data, comparativa);
+    });
+  }
+
+  // let li_lista = document.querySelectorAll("li");
+  // console.log(li_lista.length);
+
+  // for (let index = 0; index < li_lista.length; index++) {
+  //   console.log(li_lista);
+  //   li_lista.addEventListener("click", function () {
+  //     console.log("hola");
+  //     añadir_audio(data, comparativa);
+  //   });
+  // }
+}
+function añadir_audio(data, comparativa) {
+  console.log("HOLA");
+  for (let index = 0; index < data.albunes.length; index++) {
+    if (comparativa == data.albunes[index])
+      for (let x = 0; x < data.albunes[index].canciones.length; x++) {
+        // let el_audio = document.createElement("audio");
+        let el_audio;
+        el_audio.innerHTML = ` <audio id="cancion" autoplay src="./audio/${data.albunes[index].album}/${data.albunes[index].canciones[x].ruta}">
+        Texto para navegadores que no soportan la etiqueta audio...
+      </audio>`;
+        let cancion = data.albunes[index].canciones[x].ruta;
+        console.log(cancion);
+      }
+  }
+}
+
+// function reproducir() {
+//   console.log("reproduccion cancion");
+//   // tema.play();
+// }
+// function parar() {
+//   tema.pause();
+//   tema.currentTime = 0;
+//   boton_play.src = "./images/play.svg";
+// }
+
+// function controles() {
+//   let boton_play = document.querySelector("#play");
+//   boton_play.addEventListener("click", reproducir);
+
+//   document.querySelector("#stop").addEventListener("click", parar);
+// }
