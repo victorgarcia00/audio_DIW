788067a5a31745a577accd7d2af174b59a33f858
CUBASAO <dayronjosegonzalez@gmail.com>
2023-02-14 14:11:44 +0100

    Comentado de codigo completo

--- javascript/app_audio.js
+++ javascript/app_audio.js
@@ -1,74 +1,104 @@
 const button = document.querySelectorAll(".album");
-let a = new Array();
-var repro_canciones;
+// let a = new Array();
+let Album_numero;
 var caja_canciones;
 let div_caja1 = document.querySelector(".caja1 ");
-var tema = document.querySelector("#cancion");
-// tema.volume = 0.5;
+
+/*Recorrido de json  para albunes*/
 Array.from(document.querySelectorAll(".album")).forEach((element) => {
+  /* Eventos a Fotos  Album*/
   element.firstElementChild.addEventListener("click", function () {
+    /*     Obtencion de valor de texto p     */
+
     let comparativa = element.lastElementChild.innerText;
-    // let clase_lista = document.querySelector(".lista");
-    fetch("./Json/albunes.json")
-      .then((data) => data.json())
-      .then((data) => {
-        alert("Creando Reproductor de : " + comparativa);
-        for (let index = 0; index < data.albunes.length; index++) {
-          if (comparativa == data.albunes[index].album) {
-            // let repro_canciones = document.querySelector(".reproductor");
-            if (document.querySelector(".lista")) {
-              var d_nested = document.querySelector(".lista");
-              div_caja1.removeChild(d_nested);
-              var d_nested_2 = document.querySelector(".cromo");
-              while (d_nested_2.firstChild) {
-                d_nested_2.removeChild(d_nested_2.firstChild);
-              }
-              let as = document.querySelector(".reproductor");
-              var banner = document.querySelector(".crearbanner");
-              as.removeChild(banner);
-              /*Creacion reproductor cancion*/
-              crearReproductor(data, comparativa);
-              crearLista(
-                data.albunes[index].canciones.length,
-                index,
-                data,
-                div_caja1
-              );
+    /*Apertura de Fetch 1 , le pasamos el valor de p dentro de la caja de Album*/
+    Recorrido_1(comparativa);
 
-              crearbannerRepro(data, comparativa);
-              // eventos_lista(comparativa, data);
-              eventos_lista(data, comparativa);
-            } else {
-              /*Creacion reproductor cancion*/
-              crearReproductor(data, comparativa);
-              crearLista(
-                data.albunes[index].canciones.length,
-                index,
-                data,
-                div_caja1
-              );
-
-              crearbannerRepro(data, comparativa);
-              // eventos_lista(comparativa, data);
-              eventos_lista(data, comparativa);
-            }
-          }
-        }
-      });
+    /*Cierre del fetch 1*/
   });
+  /*Cierre Array de Albunes en html*/
 });
 
+function Recorrido_1(valor_p) {
+  /*Apertura de Fetch 1 */
+  fetch("./Json/albunes.json")
+    .then((data) => data.json())
+    .then((data) => {
+      /* Aviso de craecion de reproductor */
+      alert("Creando Reproductor de : " + valor_p);
+      /*Recorremos nombres de los albunes en el JSON */
+      for (let index = 0; index < data.albunes.length; index++) {
+        /*   Comparamos el valor_p con la informacion del nombre del Album */
+        if (valor_p == data.albunes[index].album) {
+          /* Guardo posicion del album para mi evento de lista */
+          Album_numero = data.albunes[index].album;
+          /*  Comprobamos si existe la clase lista den el HTML */
+          if (document.querySelector(".lista")) {
+            /* Eliminamos tanto la caratural del album como la respectiva lista y su banner */
+
+            /* Eliminacion de lista */
+            var d_nested = document.querySelector(".lista");
+            div_caja1.removeChild(d_nested);
+            /* Eliminacion de cromo/Caratula */
+            var d_nested_2 = document.querySelector(".cromo");
+            while (d_nested_2.firstChild) {
+              d_nested_2.removeChild(d_nested_2.firstChild);
+            }
+            /*Eliminacion de Banner */
+            let as = document.querySelector(".reproductor");
+            var banner = document.querySelector(".crearbanner");
+            as.removeChild(banner);
+            /*Creacion reproductor cancion*/
+            crearReproductor(data, valor_p);
+            /*Creacion de Lista */
+            crearLista(
+              data.albunes[index].canciones.length,
+              index,
+              data,
+              div_caja1
+            );
+            /* Creacion de Banner reproductor */
+            crearbannerRepro(data, valor_p);
+          } else {
+            //En caso de que no exista la clase lista simplemente creamos todo
+
+            /*Creacion reproductor cancion*/
+            crearReproductor(data, valor_p);
+            /*Creacion de Lista */
+            crearLista(
+              data.albunes[index].canciones.length,
+              index,
+              data,
+              div_caja1
+            );
+            /* Creacion de Banner reproductor */
+            crearbannerRepro(data, valor_p);
+
+            eventos_lista();
+          }
+        }
+      }
+    });
+}
+
+/* 
+Funcion que recibe los parametros del numero de canciones segun que album,
+posicion(index) del album seleccionado, pasaremos tambine el fetch (data)
+y de ultimo le pasaremos el contenedor donde va integrado el codigo de las lista */
+
 function crearLista(numero_array, index, data, div_caja1) {
+  /*Creamos div class="lista" y un titulo de album con h1  */
   caja_canciones = document.createElement("div");
   caja_canciones.classList.add("lista");
 
   let titulo = document.createElement("h1");
   titulo.setAttribute("style", "color:red;");
+  /* Creamos UL  */
   let ul_lista = document.createElement("ul");
-
-  a = numero_array;
-
-  for (let i = 0; i < a; i++) {
+  /* pasaremos el valor de la posicion de album para otra variable dentro de esta funcion */
+  contenedor_array = numero_array;
+  /* Bucle para crear tantas li como canciones existan */
+  for (let i = 0; i < contenedor_array; i++) {
     let li_lista = document.createElement("li");
     li_lista.setAttribute("style", "color:white");
     li_lista.classList.add("btn_cancion");
@@ -76,17 +106,24 @@ function crearLista(numero_array, index, data, div_caja1) {
     ul_lista.appendChild(li_lista);
   }
   titulo.append(data.albunes[index].album);
+  /* Insertamos titulo y lista en la caja_canciones */
   caja_canciones.appendChild(titulo);
   caja_canciones.appendChild(ul_lista);
-
+  /* Y a su vez agregamos en el contenedor de div_caja1 el div  caja_canciones */
   div_caja1.appendChild(caja_canciones);
 }
 
-function crearReproductor(data, comparativa) {
-  comparativa = comparativa + ".jpg";
+/* Funcion que recibe los parametros fetch (data), y el p de dentro de los Albunes
+ (en el codigo HTML) */
 
+function crearReproductor(data, campo_p) {
+  /* Concatenamos el valor de los p que tiene la Lista de ALBUM (parte de arriba) con la extension.jpg */
+  campo_p = campo_p + ".jpg";
+  /* Realizamos un bucle al json de las portadas para ver si su nombre es igual al nuestro  */
   for (let index = 0; index < data.albunes.length; index++) {
-    if (comparativa == data.albunes[index].portada) {
+    if (campo_p == data.albunes[index].portada) {
+      /* Una vez comprobado buscamos la clase "cromo" y le agregamos la imagen
+       con su respectivo titulos debajo  */
       let contenedor_cromo = document.querySelector(".cromo");
       let cromo_img = document.createElement("img");
       let cromo_p = document.createElement("h3");
@@ -97,15 +134,13 @@ function crearReproductor(data, comparativa) {
     }
   }
 }
-function crearbannerRepro() {
-  let cuerpo = document.querySelector("body");
-  let pie = document.querySelector("footer");
 
-  // let seccion = document.createElement("section");
-  // seccion.classList.add("crearbanner");
+/* Funcion que se encargara de crear nuestro div de reproduccion de musica (player)*/
+function crearbannerRepro() {
+  /* Creamos div  class="crearbanner" */
   let contenido = document.createElement("div");
   contenido.classList.add("crearbanner");
-
+  /* Insertamos el codigo Html dentro de nuestro div->contenido */
   contenido.innerHTML = ` 
   <div id="contenedor_barra">
     <h2 id="seleccionada"></h2>
@@ -135,59 +170,20 @@ function crearbannerRepro() {
   </div>
 
   `;
-
-  let a = document.querySelector(".reproductor");
-  a.appendChild(contenido);
+  /* Seleccionamos la el div donde insertaremos el reproductor y agregamos nuestro contenido 
+    que previamente habremos escrito con innerHTML*/
+  let caja_reproductor_principal = document.querySelector(".reproductor");
+  caja_reproductor_principal.appendChild(contenido);
 }
-function eventos_lista(data, comparativa) {
-  // comparativa, data
+
+/* Creacion de "solo" evento li */
+function eventos_lista() {
+  /* let  que recoje todos los li con la clase "btn_cancion" dentro del HTML  */
   let lista_accion = document.querySelectorAll(".btn_cancion");
+  /* Bucle para recorrerlo todos los li y añadirles un evento "click" */
   for (let index = 0; index < lista_accion.length; index++) {
     lista_accion[index].addEventListener("click", function () {
-      añadir_audio(data, comparativa);
+      console.log("agregando audio");
     });
   }
-
-  // let li_lista = document.querySelectorAll("li");
-  // console.log(li_lista.length);
-
-  // for (let index = 0; index < li_lista.length; index++) {
-  //   console.log(li_lista);
-  //   li_lista.addEventListener("click", function () {
-  //     console.log("hola");
-  //     añadir_audio(data, comparativa);
-  //   });
-  // }
 }
-function añadir_audio(data, comparativa) {
-  console.log("HOLA");
-  for (let index = 0; index < data.albunes.length; index++) {
-    if (comparativa == data.albunes[index])
-      for (let x = 0; x < data.albunes[index].canciones.length; x++) {
-        // let el_audio = document.createElement("audio");
-        let el_audio;
-        el_audio.innerHTML = ` <audio id="cancion" autoplay src="./audio/${data.albunes[index].album}/${data.albunes[index].canciones[x].ruta}">
-        Texto para navegadores que no soportan la etiqueta audio...
-      </audio>`;
-        let cancion = data.albunes[index].canciones[x].ruta;
-        console.log(cancion);
-      }
-  }
-}
-
-// function reproducir() {
-//   console.log("reproduccion cancion");
-//   // tema.play();
-// }
-// function parar() {
-//   tema.pause();
-//   tema.currentTime = 0;
-//   boton_play.src = "./images/play.svg";
-// }
-
-// function controles() {
-//   let boton_play = document.querySelector("#play");
-//   boton_play.addEventListener("click", reproducir);
-
-//   document.querySelector("#stop").addEventListener("click", parar);
-// }
